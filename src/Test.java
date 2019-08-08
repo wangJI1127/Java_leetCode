05f899534d7d338139611fbf9ecb2d7022dea304
wangJI1127 <wangji921x@163.com>
2019-08-08 14:14:02 +0800

    守护线程

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        char a = 'a';
+        char b = '1';
+        char c = '2';
+        int bb = b - '0' + c - '0';
+        System.out.println(bb);
+    }
+}
